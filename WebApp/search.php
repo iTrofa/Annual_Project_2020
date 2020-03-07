9325246858570520bd6b7f02db34123b9aac50cc
x0rld <thomas.sam78@gmail.com>
2020-03-07 16:25:21 +0100

    debugging search.php

--- WebApp/search.php
+++ WebApp/search.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/Affichage.css">
+    <!--<link rel="stylesheet" href="css/Affichage.css">-->
     <link rel="stylesheet" type="text/css" href="css/main.css">
 
     <title>Historique des services</title>
@@ -13,57 +13,54 @@
     <?php
       require_once 'header.php';
      ?>
-
-    <h1>Votre historique :</h1>
-    <div>
-      <?php 
+    <?php
     session_start();
-    $connected=isset($_SESSION['id'])?true:false;
+
     include('config.php');
-    
+
     $req = $db->prepare('SELECT function FROM person WHERE idPerson = ?');
     $req->execute(array($_SESSION['id']));
-      // lancement de la requête  
-    while($function = $req->fetch()){
-      $reponse[] = $function;
-      if($function['function'] == "admin" ){
-        $req = $db->prepare('SELECT dateInterv,name,category FROM log,service');
-        $req->execute(array());
-          while($admin = $req->fetch()){
-            $reponse[] = $admin;
-            echo $admin['dateInterv'] . ' ';
-            echo $admin['name'] . ' ';
-            echo $admin['category'] . ' ';
-            ?> <br> <?php
-          }  
-      }else{
 
+    // lancement de la requête
+    $res = $req->fetch();
 
-        
-      $req = $db->prepare('SELECT idLog FROM log WHERE id_person = ?');
-      $req->execute(array($_SESSION['id']));
-      while($idLog = $req->fetch()){
-        $reponse[] = $idLog;
+    if($res['function'] === "admin" ){
+        ?>
+    <h1>history of all purchases:</h1>
+    <div>
+        <?php
+        $req = $db->query('SELECT dateLog,idService FROM log');
+        $res = $req->fetchAll();
 
-        // lancement de la requête  
-        $req = $db->prepare('SELECT dateInterv,service FROM log WHERE idLog = ?');
-        $req->execute(array($idLog['idLog']));
-        while($history = $req->fetch()){
-          $reponse[] = $history;
-          echo $history['dateInterv'] . ' ' ;
-          echo $history['service'] . ' ';
-          $req = $db->prepare('SELECT name, category FROM service WHERE idService = ?');
-          $req->execute(array($history['service']));
-          while($history = $req->fetch()){
-            $reponse[] = $history;
-            echo $history['name'] . ' ';
-            echo $history['category'];
-          }
-        ?> <br> <?php
+        foreach ($res as $param)
+        {
+            $req = $db->prepare('SELECT name,category FROM service where idService = :service ');
+            $req->execute([':service'=>$param['idService']]);
+            $p = $req->fetch();
+            ?> <p><?= $param['dateLog']?>, <?=$p['name']?>,  <?= $p['category'] ?></p><br>
+        <?php
         }
+      }else{
+        ?>
+        <h1>history of all purchases:</h1>
+        <div>
+            <?php
+      $req = $db->prepare('SELECT idService,dateLog FROM log WHERE idPerson = ?');
+      $req->execute(array($_SESSION['id']));
+      $res = $req->fetchAll();
+     if(!empty($res)) {
+         foreach ($res as $param) {
+             $req = $db->prepare('SELECT name,category FROM service where idService = :service ');
+             $req->execute([':service' => $param['idService']]);
+             $p = $req->fetch();
+             ?> <p><?= $param['dateLog'] ?>, <?= $p['name'] ?>, <?= $p['category'] ?></p><br>
+             <?php
+         }
+     }
+      else{
+          echo "you didn't buy anything yet <a href='services.php'>you should buy something </a>";
       }
     }
-   } 
   ?>
     </div>
   </body>
