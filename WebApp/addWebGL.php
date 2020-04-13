ad2f3b92af329875af692d858e47237a993bb41b
Luis <luismn2000@hotmail.com>
2020-04-13 12:20:30 +0200

    Added header and footer to Demo page

--- WebApp/addWebGL.php
+++ WebApp/addWebGL.php
@@ -1,7 +1,30 @@
 <?php
+require_once "session.php";
 ?>
 <html>
-    <body>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/logo_dark.png"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
+    <link rel="stylesheet" href="css/main.css">
+    <title>Flash Assistance</title>
+    <script src="header.js"></script>
+    <script src="footer.js"></script>
+</head>
+    <body onload="checkFooter()">
+    <?php
+    require_once "header.php";
+    ?>
+    <main>
         <form method="post" enctype="multipart/form-data">
             <label>Demo Directory Name</label><br><br>
             <input type="text" name="fileDir"><br><br>
@@ -9,6 +32,10 @@
             <input type="file" name="fileName"><br><br>
             <input type="submit" name="fileSubmit" value="Submit Demo">
         </form>
+    </main>
+    <?php
+    require_once "footer.php";
+    ?>
     </body>
 </html>
 <?php
