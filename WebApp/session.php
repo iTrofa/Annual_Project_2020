f4d999f21de2f9c5ab363b6538e3fdc8eb56acce
Luis <luismn2000@hotmail.com>
2020-04-13 12:16:38 +0200

    Session manages language chosen by USER

--- WebApp/session.php
+++ WebApp/session.php
@@ -1,8 +1,18 @@
 <?php
+require_once "DbManager.php";
+$DbManager = new DbManager();
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 if (!isset($_SESSION['id'])){
     header('location: login.php');
     exit;
+}
+$q = $DbManager->getDb()->prepare("SELECT lang from Person WHERE idPerson = ?");
+$q->execute([
+   $_SESSION['id']
+]);
+$res = $q->fetchAll();
+if($res[0]['lang'] != "en_US"){
+    $_SESSION['lang'] = $res[0]['lang'];
 }
